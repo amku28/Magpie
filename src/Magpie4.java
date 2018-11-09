9699b1d5cfbb3f454c9bba435ef0880354ac4440
Alexander Ku <kualexcw28@gmail.com>
2018-11-09 07:45:41 -0700

    Finish activity 4

--- src/Magpie4.java
+++ src/Magpie4.java
@@ -48,6 +48,14 @@ public class Magpie4 {
 			response = transformIWantToStatement(statement);
 		}
 
+		else if (findKeyword(statement, "I want", 0) >= 0) {
+			response = transformIWantStatement(statement);
+		}
+
+		else if (findKeyword(statement, "I", 0) >= 0) {
+			response = transformIYouStatement(statement);
+		}
+ 
 		else {
 			// Look for a two word (you <something> me)
 			// pattern
@@ -80,6 +88,45 @@ public class Magpie4 {
 		int psn = findKeyword(statement, "I want to", 0);
 		String restOfStatement = statement.substring(psn + 9).trim();
 		return "What would it mean to " + restOfStatement + "?";
+	}
+
+	/**
+	 * Take a statement with "I want <something>. and transform it into
+	 * "Would you really be happy if you had <something>?"
+	 *
+	 * @param statement
+	 * 				the user statement, assumed to contain "I want"
+	 * @return the transformed statement
+	 */
+	private String transformIWantStatement(String statement) {
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword(statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";
+	}
+
+	/**
+	 * Take a statement with "I <something> you". and transform it into
+	 * "Why do you <something> me?"
+	 * 
+	 * @param statement
+	 * 				the user statement, assumed to contain "I"
+	 * @return the transformed statment
+	 */
+	private String transformIYouStatement(String statement) {
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword(statement, "I", 0);
+		String restOfStatement = statement.substring(psn + 1).trim();
+		restOfStatement = restOfStatement.substring(0, restOfStatement.length() - 4);
+		return "Why do you " + restOfStatement + " me?";
 	}
 
 	/**
